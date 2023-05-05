0974965721901ae0a306cd8fc22413d66e775068
Muhammad Usman Javed <musmanjaved5552@gmail.com>
2023-05-05 12:14:15 +0500

    sajid new web

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SK Agency</h1>
+      <div className="card">
+        <p>We provide all kind of social media services</p>
+        <h2>CEO</h2>
+        <p>Sajid Rahman</p>
+        <h2>Contact</h2>
+        <p>ownusemailbox@gmail.com</p>
+        <p>+923326077343</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
